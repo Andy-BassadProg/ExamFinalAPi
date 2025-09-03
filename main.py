7b92ddb07b6a8daac776bccee13273cbd8412869
Andy-BassadProg <hei.tojotiana@gmail.com>
2025-09-03 11:52:54 +0300

    feat : b

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
+
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+cars = []
+
+@app.get("/ping")
+def ping():
+    return "pong"
+
+@app.post("/cars", status_code=201)
+def create_cars(new_cars: list[Car]):
+    cars.extend(new_cars)
+    return {
+        "message": f"{len(new_cars)} voiture ajouté",
+        "total": len(cars)
+    }
+
+@app.get("/cars")
+def get_cars():
+    return cars
